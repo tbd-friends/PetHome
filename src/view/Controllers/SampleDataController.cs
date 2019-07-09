59e9f5702e4dfb226ad7864e87920c03971559a1
Terry Burns-Dyson <terrybd@gmail.com>
2019-07-08 22:42:03 -0400

    Moved app project to view
    Agreed app was view, namespaced as PetHome.View, refactored and moved
    project.
    
    Created an sln to be able to open in VS. Verified that the project will
    build and run from within VS code still.

--- src/view/Controllers/SampleDataController.cs
+++ src/view/Controllers/SampleDataController.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace app.Controllers
+namespace PetHome.View.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
