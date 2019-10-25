f15239be4e545d70cb47060fd72df5f27bd435bb
Nelson Segarra <junicus@outlook.com>
2019-10-24 22:21:04 -0400

    reverted changes to controller

--- src/view/Controllers/AnimalQueriesController.cs
+++ src/view/Controllers/AnimalQueriesController.cs
@@ -24,7 +24,6 @@ namespace view.Controllers
         [HttpGet]
         public async Task<IEnumerable<AnimalSummary>> Get()
         {
-            await Task.Delay(5000);
             return await _mediator.Send(new GetAnimals());
         }
 
